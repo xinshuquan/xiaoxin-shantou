82438a0f5edf40045227144447b581ff77132364
xinshuquan <xinshuquan@github.com>
2026-03-12 18:19:22 +0800

    fix: change button text to 登录

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -105,7 +105,7 @@ export default function Header() {
                 <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                 </svg>
-                <span>游客登录</span>
+                <span>登录</span>
               </Link>
 
               {/* Mobile Menu Toggle */}
